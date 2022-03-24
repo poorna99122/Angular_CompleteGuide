fed716a2e0ba1fb70f08c48d0e8702482f2e51cf
poorna99122 <poornachandrarao99122@gmail.com>
2022-03-23 19:00:19 -0700

    Added chart.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+// import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    
   ],
   imports: [
     BrowserModule,
