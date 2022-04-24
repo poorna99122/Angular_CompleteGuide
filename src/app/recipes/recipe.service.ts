2ce76aa3df1dd2608566e7e8b99e87afcbbd1283
poorna99122 <poornachandrarao99122@gmail.com>
2022-04-24 15:10:41 -0700

    We have added ingredients array in recipe.model.ts and we are displaying that ingredient in recipeDetail component getting from recipe model class and we are puushing that data from recipeService class

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from "./recipe.model";
 
 //Recipe Service is the place in our app where we manage our recipes
@@ -8,9 +9,34 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
 private recipes: Recipe[] = [
-    new Recipe('Biryani','Biryani is one of the most popular dishes in South Asia, as well as among the diaspora from the region. ','https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Biryani_of_Lahore.jpg/220px-Biryani_of_Lahore.jpg'),
-    new Recipe('Pizza','Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven.','https://www.vegrecipesofindia.com/wp-content/uploads/2020/11/pizza-recipe-1152x1536.jpg'),
-    new Recipe('Burger','A hamburger is a food consisting of fillings —usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll. ','https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/1:1/w_1600,c_limit/Smashburger-recipe-120219.jpg')
+    new Recipe(
+        'Biryani',
+        'Biryani is one of the most popular dishes in South Asia, as well as among the diaspora from the region. ',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Biryani_of_Lahore.jpg/220px-Biryani_of_Lahore.jpg',
+        [
+            new Ingredient('Rice',1),
+            new Ingredient('Meat',1)
+
+        ]),
+        
+    new Recipe(
+        'Pizza',
+        'Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven.',
+        'https://www.vegrecipesofindia.com/wp-content/uploads/2020/11/pizza-recipe-1152x1536.jpg',
+        [
+            new Ingredient('Flour',2),
+            new Ingredient('Meat',1)
+
+        ]),
+        
+    new Recipe(
+        'Burger',
+        'A hamburger is a food consisting of fillings —usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll. ',
+        'https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/1:1/w_1600,c_limit/Smashburger-recipe-120219.jpg',
+        [
+            new Ingredient('Buns',2),
+            new Ingredient('Meat',1)
+        ])
 
    ];
 
